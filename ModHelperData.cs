0d863279bec24323054fbcd088a9aebba78d2de8
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2024-02-07 20:33:07 -0500

    Update ModHelperData.cs
    Fixed typos

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,9 +6,9 @@ public static class ModHelperData
     public const string Version = "1.1.0";
     public const string Name = "Awesomer MonkeySub Paragon";
 
-    public const string Description = "Makes the Monkey Sub Paragon good. Or maybe.. too good. Massivley increased range, grants all submerge effects while unsubmerged, including the radiation attack and sub buffs. Grants all towers camo. Additionally the ability has only a 3 second wind-up and massively increased damage to justify the 2 minute cooldown";
+    public const string Description = "Makes the Monkey Sub Paragon good. Or maybe... too good. Massively increased range, grants all submerge effects while unsubmerged, including the radiation attack and sub buffs. Grants all towers camo. Additionally the ability has only a 3 second wind-up and massively increased damage to justify the 2 minute cooldown";
 
-    public const string Icon = "";
+    public const string Icon = " ";
     public const string RepoOwner = "DatJaneDoe";
     public const string DllName = "AwesomerSub.dll";
 	
