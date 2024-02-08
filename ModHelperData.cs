a8f3ac9884dd437b1f08a1fa4a59ab6b477a9779
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2024-02-07 20:26:39 -0500

    Added ModHelperData

---
+++ ModHelperData.cs
@@ -0,0 +1,15 @@
+namespace AwesomerSub;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "41.0";
+    public const string Version = "1.1.0";
+    public const string Name = "DJDTowerPack";
+
+    public const string Description = "Makes the Monkey Sub Paragon good. Or maybe.. too good. Massivley increased range, grants all submerge effects while unsubmerged, including the radiation attack and sub buffs. Grants all towers camo. Additionally the ability has only a 3 second wind-up and massively increased damage to justify the 2 minute cooldown";
+
+    public const string Icon = "";
+    public const string RepoOwner = "DatJaneDoe";
+    public const string DllName = "AwesomerSub.dll";
+	
+}
